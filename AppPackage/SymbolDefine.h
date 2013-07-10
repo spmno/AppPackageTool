9510d8382dfc3a68c00bb8130c92330a788ee42f
spmno <sunqingpeng@hotmail.com>
2013-07-10 09:45:53 +0800

    split the define to whole project.

---
+++ AppPackage/SymbolDefine.h
@@ -0,0 +1,8 @@
+#ifndef APPPACKAGE_SYMBOLDEFINE_H
+#define APPPACKAGE_SYMBOLDEFINE_H
+
+#define MD5_KEY_MAX_LENGTH							64
+#define UPDATE_METHOD "UpdateMethod"
+#define MD5_CONTENT "MD5Content"
+
+#endif
